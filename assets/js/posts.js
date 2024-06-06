e30ceb18f8dffa8f5d2e011c7a10c7f7303e2392
Daniel D <ddunnemann@gmail.com>
2024-06-06 11:28:30 -0500

    Post functionality seems to be working at this point. Next would be to tackle light and dark mode.

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,6 +1,10 @@
 const returnButton = document.querySelector('#return');
+const postContainer = document.querySelector('main')
+
 
 posts = JSON.parse(localStorage.getItem('posts'));
+
+
 
 for (let i = 0; i < posts.length; i++) {
 console.log(
@@ -11,6 +15,21 @@ SUBJECT: ${posts[i].title}
 MESSAGE: ${posts[i].content}
 ${posts[i].mm+1}/${posts[i].dd}/${posts[i].yy}, ${posts[i].hh}:${posts[i].tt} 
 `);
+
+const article = document.createElement('article');
+const postSubject = document.createElement('div')
+const postHandleDate = document.createElement('div')
+const postContent = document.createElement('div')
+postSubject.className = 'subject';
+postHandleDate.className = 'handle-date';
+postContent.className = 'post-content';
+postSubject.textContent = posts[i].title;
+postHandleDate.textContent = `by ${posts[i].poster} on ${posts[i].mm+1}/${posts[i].dd}/${posts[i].yy}, ${posts[i].hh}:${posts[i].tt}`;
+postContent.textContent = posts[i].content;
+article.appendChild(postSubject);
+article.appendChild(postHandleDate);
+article.appendChild(postContent);
+postContainer.appendChild(article);
 };
 
 returnButton.addEventListener('click', function (event) {
